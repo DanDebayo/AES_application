2246dec859839c9b74e4865fb8ed4a8cf7feee88
Dandebayo <olasubomidadebayo@gmail.com>
2023-03-29 16:39:58 -0700

    Added more code for the applciation. currently having an issue finding the dll library

--- AES.py
+++ AES.py
@@ -21,3 +21,27 @@ def decrypt():
     output_data = aes_lib.aes_decrypt(input_data, b"24680")
     output_text.delete("1.0", "end")
     output_text.insert("1.0", output_data.decode())
+
+
+root = tk.Tk()
+root.title("AES Encryption/Decryption")
+
+input_label = tk.Label(root, text="Input:")
+input_label.pack()
+
+input_text = tk.Text(root, height=5)
+input_text.pack()
+
+encrypt_button = tk.Button(root, text="Encrypt", command=encrypt)
+encrypt_button.pack()
+
+decrypt_button = tk.Button(root, text="Decrypt", command=decrypt)
+decrypt_button.pack()
+
+output_label = tk.Label(root, text="Output:")
+output_label.pack()
+
+output_text = tk.Text(root, height=5)
+output_text.pack()
+
+root.mainloop()
