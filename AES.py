dbafd6f25bdc1e8efd440b5925541093bcea78e6
Dandebayo <olasubomidadebayo@gmail.com>
2023-03-30 11:05:22 -0700

    Made changes to the pyhton application to make it work and alot more specific, with a change to shared library as i forgot dll won't work for linux and it should be so. will be posting the so file after

--- AES.py
+++ AES.py
@@ -1,47 +1,57 @@
-import tkinter as tk
 import ctypes
+import os
+import tkinter as tk
+from tkinter import filedialog
 
-aes_lib = ctypes.cdll.LoadLibrary('aes.dll')
+aes_lib_path = os.path.abspath('libaes.so')
+aes_lib = ctypes.cdll.LoadLibrary(aes_lib_path)
 
+encrypt_func = aes_lib['_Z7EncryptRKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEE']
+decrypt_func = aes_lib['_Z7DecryptB5cxx11St6vectorIhSaIhEE']
 
-aes_lib.aes_encrypt.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
-aes_lib.aes_encrypt.restype = ctypes.c_char_p
-aes_lib.aes_decrypt.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
-aes_lib.aes_decrypt.restype = ctypes.c_char_p
+encrypt_func.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_void_p]
+encrypt_func.restype = ctypes.c_void_p
+decrypt_func.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_void_p]
+decrypt_func.restype = ctypes.c_void_p
 
+def encrypt_file():
+    input_file = filedialog.askopenfilename()
+    if not input_file:
+        return
+    with open(input_file, 'rb') as f:
+        input_data = f.read()
+    output_file = filedialog.asksaveasfilename()
+    if not output_file:
+        return
+    output_data = ctypes.create_string_buffer(len(input_data))
+    encrypt_func(input_data, b"24680", output_data)
+    with open(output_file, 'wb') as f:
+        f.write(output_data)
 
-def encrypt():
-    input_data = input_text.get("1.0", "end-1c").encode()
-    output_data = aes_lib.aes_encrypt(input_data, b"24680")
-    output_text.delete("1.0", "end")
-    output_text.insert("1.0", output_data.decode())
-
-def decrypt():
-    input_data = input_text.get("1.0", "end-1c").encode()
-    output_data = aes_lib.aes_decrypt(input_data, b"24680")
-    output_text.delete("1.0", "end")
-    output_text.insert("1.0", output_data.decode())
-
+def decrypt_file():
+    input_file = filedialog.askopenfilename()
+    if not input_file:
+        return
+    with open(input_file, 'rb') as f:
+        input_data = f.read()
+    output_file = filedialog.asksaveasfilename()
+    if not output_file:
+        return
+    output_data = ctypes.create_string_buffer(len(input_data))
+    decrypt_func(input_data, b"24680", ctypes.byref(output_data))
+    with open(output_file, 'wb') as f:
+        f.write(output_data)
 
 root = tk.Tk()
 root.title("AES Encryption/Decryption")
 
-input_label = tk.Label(root, text="Input:")
+input_label = tk.Label(root, text="Choose an option:")
 input_label.pack()
 
-input_text = tk.Text(root, height=5)
-input_text.pack()
-
-encrypt_button = tk.Button(root, text="Encrypt", command=encrypt)
+encrypt_button = tk.Button(root, text="Encrypt file", command=encrypt_file)
 encrypt_button.pack()
 
-decrypt_button = tk.Button(root, text="Decrypt", command=decrypt)
+decrypt_button = tk.Button(root, text="Decrypt file", command=decrypt_file)
 decrypt_button.pack()
-
-output_label = tk.Label(root, text="Output:")
-output_label.pack()
-
-output_text = tk.Text(root, height=5)
-output_text.pack()
 
 root.mainloop()
