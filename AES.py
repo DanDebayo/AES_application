7a0eb29009a140d3b19f3bd4fb3bcfef7d632061
Dandebayo <olasubomidadebayo@gmail.com>
2023-04-07 11:56:44 -0700

    Made a few updates in the past few day to understand why the transfer of strings from the python gui to the c++ function is not work. Currently still working on it

--- AES.py
+++ AES.py
@@ -1,7 +1,8 @@
-import ctypes
+import array
 import os
 import tkinter as tk
 from tkinter import filedialog
+import ctypes
 
 aes_lib_path = os.path.abspath('libaes.so')
 aes_lib = ctypes.cdll.LoadLibrary(aes_lib_path)
@@ -9,8 +10,8 @@ aes_lib = ctypes.cdll.LoadLibrary(aes_lib_path)
 encrypt_func = aes_lib['_Z7EncryptRKNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEE']
 decrypt_func = aes_lib['_Z7DecryptB5cxx11St6vectorIhSaIhEE']
 
-encrypt_func.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_void_p]
-encrypt_func.restype = ctypes.c_void_p
+encrypt_func.argtypes = [ctypes.POINTER(ctypes.c_uint8), ctypes.c_int, ctypes.POINTER(ctypes.c_uint8)]
+encrypt_func.restype = None
 decrypt_func.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_void_p]
 decrypt_func.restype = ctypes.c_void_p
 
@@ -23,8 +24,12 @@ def encrypt_file():
     output_file = filedialog.asksaveasfilename()
     if not output_file:
         return
-    output_data = ctypes.create_string_buffer(len(input_data))
-    encrypt_func(input_data, b"24680", output_data)
+    input_len = len(input_data)
+    output_len = (input_len + 15) & ~15
+    input_array = (ctypes.c_uint8 * input_len).from_buffer_copy(input_data)
+    output_array = (ctypes.c_uint8 * output_len)()
+    encrypt_func(input_array, input_len, output_array)
+    output_data = bytes(output_array[:input_len])
     with open(output_file, 'wb') as f:
         f.write(output_data)
 
